96bde80b14852d3a706ba23ebc1191b3ffcbc660
Asrith Tanniru <asrithtanniru155@gmail.com>
2026-02-25 22:25:38 +0530

    added assets

--- game/src/game/main.ts
+++ game/src/game/main.ts
@@ -1,31 +1,42 @@
 import { Boot } from './scenes/Boot';
 import { GameOver } from './scenes/GameOver';
-import { Game as MainGame } from './scenes/Game';
 import { MainMenu } from './scenes/MainMenu';
-import { AUTO, Game } from 'phaser';
 import { Preloader } from './scenes/Preloader';
+import { Act1Scene } from './scenes/Act1Scene';
+import { Act2Scene } from './scenes/Act2Scene';
+import { Act3Scene } from './scenes/Act3Scene';
+import { AUTO, Game } from 'phaser';
 
-//  Find out more information about the Game Config at:
-//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Phaser.Types.Core.GameConfig = {
     type: AUTO,
     width: 1024,
     height: 768,
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    backgroundColor: '#05070B',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x: 0, y: 0 },
+            debug: false,
+        },
+    },
     scene: [
         Boot,
         Preloader,
         MainMenu,
-        MainGame,
-        GameOver
-    ]
+        Act1Scene,
+        Act2Scene,
+        Act3Scene,
+        GameOver,
+    ],
 };
 
 const StartGame = (parent: string) => {
-
     return new Game({ ...config, parent });
-
-}
+};
 
 export default StartGame;
--- game/src/game/scenes/Preloader.ts
+++ game/src/game/scenes/Preloader.ts
@@ -9,39 +9,53 @@ export class Preloader extends Scene
 
     init ()
     {
-        //  We loaded this image in our Boot Scene, so we can display it here
-        this.add.image(512, 384, 'background');
+        // Dark tinted background
+        const bg = this.add.image(512, 384, 'background');
+        bg.setTint(0x222233);
 
-        //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+        // Title while loading
+        this.add.text(512, 280, 'THE QUIET PROTOCOL', {
+            fontFamily: '"Courier New", monospace',
+            fontSize: '28px',
+            color: '#8899bb',
+            letterSpacing: 6
+        }).setOrigin(0.5);
 
-        //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+        // Loading bar outline
+        this.add.rectangle(512, 384, 400, 8).setStrokeStyle(1, 0x445577);
 
-        //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
+        // Loading bar fill
+        const bar = this.add.rectangle(512 - 198, 384, 4, 6, 0x6688cc);
+
         this.load.on('progress', (progress: number) => {
-
-            //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-            bar.width = 4 + (460 * progress);
-
+            bar.width = 4 + (396 * progress);
         });
+
+        // Loading status text
+        this.add.text(512, 410, 'INITIALIZING NEURAL ARCHIVE...', {
+            fontFamily: '"Courier New", monospace',
+            fontSize: '12px',
+            color: '#556688'
+        }).setOrigin(0.5);
     }
 
     preload ()
     {
-        //  Load the assets for the game - Replace with your own assets
         this.load.setPath('assets');
 
-        this.load.image('logo', 'logo.png');
-        this.load.image('star', 'star.png');
+        // Environment backgrounds
+        this.load.image('hallway-bg', 'TQP_A1_ENV_Hallway_Base.png');
+        this.load.image('trauma-bg', 'TQP_A1_ENV_TraumaRoom_Base.png');
+        this.load.image('neurocore-bg', 'TQP_AF_ENV_NeuroCore_Base.png');
+        this.load.image('security-tiles', 'TQP_A1_TILE_SecurityRoom_Modular.png');
+
+        // Character sprites
+        this.load.image('aarav', 'TQP_NPC_Aarav_Idle.png');
+        this.load.image('janitor', 'TQP_A3_NPC_Janitor_Idle.png');
     }
 
     create ()
     {
-        //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
-        //  For example, you can define global animations here, so we can use them in other scenes.
-
-        //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
         this.scene.start('MainMenu');
     }
 }
